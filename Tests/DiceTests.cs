3b6b402b3ca29ba5a074ae34e6d880cc7fdf8eb4
KingFruit85 <christopher.aaron.long@gmail.com>
2020-09-19 18:44:05 +0100

    Refactored tests to only one assertion per test as per best practices

--- Tests/DiceTests.cs
+++ Tests/DiceTests.cs
@@ -9,116 +9,108 @@ namespace DiceTests
         public void D20ReturnsValidInt()
         {
             // Arrange
-            Dice.DiceTypes D20 = new DiceTypes();
-
-            // Act
-            var result = D20.RollD20();
-
+            var D20 = new DiceTypes().RollD20();
             // Assert
+            Assert.IsType<int>(D20[0][0]);
+        }
 
-            // Checks that result is of type int
-            Assert.IsType<int>(result[0][0]);
-
-            // Checks that result is within valid range
-            Assert.InRange(result[0][0],1,20);
-            
+        [Fact]
+        public void D20ReturnsWithinRange()
+        {
+            // Arrange
+            var D20 = new DiceTypes().RollD20();
+            // Assert
+            Assert.InRange(D20[0][0],1,20);
         }
 
         [Fact]
         public void D12ReturnsValidInt()
         {
             // Arrange
-            Dice.DiceTypes D12 = new DiceTypes();
-            
-            // Act
-            var result = D12.RollD12();
-
+            var D12 = new DiceTypes().RollD12();
             // Assert
+            Assert.IsType<int>(D12[0][0]);
+        }
 
-            // Checks that result is of type int
-            Assert.IsType<int>(result[0][0]);
-
-            // Checks that result is within valid range
-            Assert.InRange(result[0][0],1,12);
-            
+        [Fact]
+        public void D12ReturnsWithinRange()
+        {
+            // Arrange
+            var D12 = new DiceTypes().RollD12();;
+            // Assert
+            Assert.InRange(D12[0][0],1,12);
         }
 
         [Fact]
         public void D10ReturnsValidInt()
         {
             // Arrange
-            Dice.DiceTypes D10 = new DiceTypes();
-            
-            // Act
-            var result = D10.RollD10();
-
+            var D10 = new DiceTypes().RollD10();
             // Assert
+            Assert.IsType<int>(D10[0][0]);
+        } 
 
-            // Checks that result is of type int
-            Assert.IsType<int>(result[0][0]);
-
-            // Checks that result is within valid range
-            Assert.InRange(result[0][0],1,10);
-            
-        }    
+        [Fact]
+        public void D10ReturnsWithinRange()
+        {
+            // Arrange
+            var D10 = new DiceTypes().RollD10();
+            // Assert
+            Assert.InRange(D10[0][0],1,10);
+        }  
 
         [Fact]
         public void D8ReturnsValidInt()
         {
             // Arrange
-            Dice.DiceTypes D8 = new DiceTypes();
-            
-            // Act
-            var result = D8.RollD8();
-
+            var D8 = new DiceTypes().RollD8();
             // Assert
+            Assert.IsType<int>(D8[0][0]);
+        }
 
-            // Checks that result is of type int
-            Assert.IsType<int>(result[0][0]);
-
-            // Checks that result is within valid range
-            Assert.InRange(result[0][0],1,8);
-            
+        [Fact]
+        public void D8ReturnsWithinRange()
+        {
+            // Arrange
+            var D8 = new DiceTypes().RollD8();
+            // Assert
+            Assert.InRange(D8[0][0],1,8);
         }
 
         [Fact]
         public void D4ReturnsValidInt()
         {
             // Arrange
-            Dice.DiceTypes D4 = new DiceTypes();
-            
-            // Act
-            var result = D4.RollD4();
-
+            var D4 = new DiceTypes().RollD4();
             // Assert
+            Assert.IsType<int>(D4[0][0]);
+        }
 
-            // Checks that result is of type int
-            Assert.IsType<int>(result[0][0]);
-
-            // Checks that result is within valid range
-            Assert.InRange(result[0][0],1,4);
-            
+        [Fact]
+        public void D4ReturnsWithinRange()
+        {
+            // Arrange
+            var D4 = new DiceTypes().RollD4();
+            // Assert
+            Assert.InRange(D4[0][0],1,4);
         }
 
         [Fact]
         public void SixAbilitiyScoresReturned()
         {
             // Arrange
-            Dice.DiceTypes x = new DiceTypes();
-            
-            // Act
-            var result = x.ReturnAbilityScores();
-
+            var scores = new DiceTypes().ReturnAbilityScores();
             // Assert
+            Assert.Equal(6,scores.Length);
+        }
 
-            // Checks that array is not empty
-            Assert.NotEmpty(result);
-
-            // Checks that array is correct size
-            Assert.Equal(6,result.Length);
-
-
-            
+        [Fact]
+        public void SixAbilitiyScoresNotNull()
+        {
+            // Arrange
+            var scores = new DiceTypes().ReturnAbilityScores();
+            // Assert
+            Assert.NotNull(scores);
         }
     }
     
--- Tests/HumanTests.cs
+++ Tests/HumanTests.cs
@@ -10,114 +10,244 @@ namespace HumanTests
         {
             // Arrange
             var Lang = new Human().GetLanguages();
-
             // Act
             var actual = Lang.Count;
-
             // Assert
             // Humans know two languages at level 1
             // One being Common, the other random
             Assert.Equal(2,actual);
+        }
+
+        [Fact]
+        public void HumanLanguageIncludesCommon()
+        {
+            // Arrange
+            var Lang = new Human().GetLanguages();
+            // Assert
             Assert.Contains("Common", Lang);
         }
 
         [Fact]
-        public void HumanAlignmentNotNullAndCorrectType()
+        public void HumanAlignmentNotNull()
         {
             // Arrange
             var Alignment = new Human().GetAlignment();
-            
             //Assert
             Assert.NotNull(Alignment);
-            Assert.IsType(typeof(string), Alignment);
         }
 
         [Fact]
-        public void AgeNotNullAndCorrectType()
+        public void HumanAlignmentIsCorrectType()
+        {
+            // Arrange
+            var Alignment = new Human().GetAlignment();
+            //Assert
+            Assert.IsType(typeof(string), Alignment);
+        }
+
+
+
+        [Fact]
+        public void AgeNotNull()
         {
             //Arrange
             var Age = new Human().GetAge();
-
             // Assert
             Assert.NotNull(Age);
+        }
+
+        [Fact]
+        public void AgeCorrectType()
+        {
+            //Arrange
+            var Age = new Human().GetAge();
+            // Assert
             Assert.IsType(typeof(int),Age);
+        }
+
+        [Fact]
+        public void AgeInHUmanRange()
+        {
+            //Arrange
+            var Age = new Human().GetAge();
+            // Assert
             Assert.InRange(Age,18,65);
         }
 
         [Fact]
-        public void SizeIsNotNullAndCorrectType()
+        public void SizeIsNotNull()
         {
             // Arrange 
             var Size = new Human().GetSize();
-
             // Assert
             Assert.NotNull(Size);
+        }
+
+        [Fact]
+        public void SizeIsCorrectType()
+        {
+            // Arrange 
+            var Size = new Human().GetSize();
+            // Assert
             Assert.IsType(typeof(string),Size);
         }
 
         [Fact]
-        public void SpeedIsNotNullAndCorrectType()
+        public void SpeedIsNotNull()
         {
             // Arrange 
             var Size = new Human().GetSpeed();
-
             // Assert
             Assert.NotNull(Size);
+        }
+
+        [Fact]
+        public void SpeedIsCorrectType()
+        {
+            // Arrange 
+            var Size = new Human().GetSpeed();
+            // Assert
             Assert.IsType(typeof(int), Size);
         }
 
         [Fact]
-        public void AbilityScoreIncreasesNotNullAndCorrectValue()
+        public void STRAbilityScoreNotNull()
         {
-            // Arrange
+            // Arrange 
             var Scores = new Human().GetAbilityScoreIncrease();
             // Act
-
-            int STRResult;
-            var STR = Scores.TryGetValue("STR", out STRResult);
-            var STRValue = Scores["STR"];
-
-            int DEXResult;
-            var DEX = Scores.TryGetValue("DEX", out DEXResult);
-            var DEXValue = Scores["DEX"];
-
-            int CONResult;
-            var CON = Scores.TryGetValue("CON", out CONResult);
-            var CONValue = Scores["CON"];
-
-            int INTResult;
-            var INT = Scores.TryGetValue("INT", out INTResult);
-            var INTValue = Scores["INT"];
-
-            int CHAResult;
-            var CHA = Scores.TryGetValue("CHA", out CHAResult);
-            var CHAValue = Scores["CHA"];
-
-            int WISResult;
-            var WIS = Scores.TryGetValue("WIS", out WISResult);
-            var WISValue = Scores["WIS"];
-
+            var STR = Scores.TryGetValue("STR", out var STRResult);
             // Assert
-
             Assert.True(STR);
+        }
+
+        [Fact]
+        public void DEXAbilityScoreNotNull()
+        {
+            // Arrange 
+            var Scores = new Human().GetAbilityScoreIncrease();
+            // Act
+            var DEX = Scores.TryGetValue("DEX", out var DEXResult);
+            // Assert
+            Assert.True(DEX);
+        }
+
+        [Fact]
+        public void CONAbilityScoreNotNull()
+        {
+            // Arrange 
+            var Scores = new Human().GetAbilityScoreIncrease();
+            // Act
+            var CON = Scores.TryGetValue("CON", out var CONResult);
+            // Assert
+            Assert.True(CON);
+        }
+
+        [Fact]
+        public void INTAbilityScoreNotNull()
+        {
+            // Arrange 
+            var Scores = new Human().GetAbilityScoreIncrease();
+            // Act
+            var INT = Scores.TryGetValue("INT", out var INTResult);
+            // Assert
+            Assert.True(INT);
+        }
+
+        [Fact]
+        public void CHAAbilityScoreNotNull()
+        {
+            // Arrange 
+            var Scores = new Human().GetAbilityScoreIncrease();
+            // Act
+            var CHA = Scores.TryGetValue("CHA", out var CHAResult);
+            // Assert
+            Assert.True(CHA);
+        }
+
+        [Fact]
+        public void WISAbilityScoreNotNull()
+        {
+            // Arrange 
+            var Scores = new Human().GetAbilityScoreIncrease();
+            // Act
+            var WIS = Scores.TryGetValue("WIS", out var WISResult);
+            // Assert
+            Assert.True(WIS);
+        }
+
+        [Fact]
+        public void STRAbilityScoreIncreaseEquals1()
+        {
+            // Arrange 
+            var Scores = new Human().GetAbilityScoreIncrease();
+            // Act
+            var STRValue = Scores["STR"];
+            // Assert
             Assert.Equal(STRValue,1);
 
-            Assert.True(DEX);
+        }
+
+        [Fact]
+        public void DEXAbilityScoreIncreaseEquals1()
+        {
+            // Arrange 
+            var Scores = new Human().GetAbilityScoreIncrease();
+            // Act
+            var DEXValue = Scores["DEX"];
+            // Assert
             Assert.Equal(DEXValue,1);
 
-            Assert.True(CON);
+        }
+
+        [Fact]
+        public void CONAbilityScoreIncreaseEquals1()
+        {
+            // Arrange 
+            var Scores = new Human().GetAbilityScoreIncrease();
+            // Act
+            var CONValue = Scores["CON"];
+            // Assert
             Assert.Equal(CONValue,1);
 
-            Assert.True(INT);
+        }
+
+        [Fact]
+        public void INTAbilityScoreIncreaseEquals1()
+        {
+            // Arrange 
+            var Scores = new Human().GetAbilityScoreIncrease();
+            // Act
+            var INTValue = Scores["INT"];
+            // Assert
             Assert.Equal(INTValue,1);
 
-            Assert.True(CHA);
+        }
+
+        [Fact]
+        public void CHAAbilityScoreIncreaseEquals1()
+        {
+            // Arrange 
+            var Scores = new Human().GetAbilityScoreIncrease();
+            // Act
+            var CHAValue = Scores["CHA"];
+            // Assert
             Assert.Equal(CHAValue,1);
 
-            Assert.True(WIS);
-            Assert.Equal(WISValue,1);
-            
         }
+
+        [Fact]
+        public void WISAbilityScoreIncreaseEquals1()
+        {
+            // Arrange 
+            var Scores = new Human().GetAbilityScoreIncrease();
+            // Act
+            var WISValue = Scores["WIS"];
+            // Assert
+            Assert.Equal(WISValue,1);
+
+        }
+
     }
     
 }
